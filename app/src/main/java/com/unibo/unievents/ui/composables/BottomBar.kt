f920c6d7af8edd711a6b1df860e6e1ffe29e128e
Matt <matteo.crepaldi04@gmail.com>
2026-04-22 12:41:37 +0200

    First implementation for the navigation bar

---
+++ app/src/main/java/com/unibo/unievents/ui/composables/BottomBar.kt
@@ -0,0 +1,47 @@
+package com.unibo.unievents.ui.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BottomBar() {
+    BottomAppBar(
+        actions = {
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                IconButton(
+                    onClick = { /* TODO */ }
+                ) {
+                    Icon(Icons.Filled.Home, "Home")
+                }
+
+                IconButton(onClick = { /* TODO */ }) {
+                    Icon(Icons.Filled.LocationOn, "Map")
+                }
+
+                IconButton(onClick = { /* TODO */ }) {
+                    Icon(Icons.Filled.DateRange, "Calendar")
+                }
+
+                IconButton(onClick = { /* TODO */ }) {
+                    Icon(Icons.Filled.Person, "User")
+                }
+            }
+        }
+    )
+}
