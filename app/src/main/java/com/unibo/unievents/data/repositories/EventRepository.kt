124eef31f00f42f25962b6031792e2f43e288867
matteo.crepaldi04 <matteo.crepaldi04@gmail.com>
2026-05-04 14:15:44 +0200

    Home page now pulls only approved events

--- app/src/main/java/com/unibo/unievents/data/repositories/EventRepository.kt
+++ app/src/main/java/com/unibo/unievents/data/repositories/EventRepository.kt
@@ -1,17 +1,45 @@
 package com.unibo.unievents.data.repositories
 
 import com.unibo.unievents.data.Event
+import com.unibo.unievents.data.EventInsert
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.postgrest.from
+import io.github.jan.supabase.postgrest.postgrest
+import io.github.jan.supabase.postgrest.query.Columns
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalTime
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class EventId (
+    @SerialName("id") val id: Int
+)
 
 class EventRepository(private val supabase: SupabaseClient) {
-    suspend fun getEvents(): List<Event> {
-        return supabase.from("events")
+    suspend fun getApprovedEvents(): List<Event> {
+        return supabase.postgrest["events"]
+            .select(columns = Columns.raw("*, approved_events!inner(id)"))
+            .decodeList<Event>()
+    }
+
+    suspend fun getPendingEvents(): List<Event> {
+        return supabase.from("pending_events")
             .select()
             .decodeList<Event>()
     }
 
-    suspend fun createEvent(event: Event): Result<Unit> {
+    suspend fun deleteEvent(event: Event) {
+        supabase.postgrest["events"]
+            .delete { filter { eq("id", event.id) } }
+    }
+
+    suspend fun approveEvent(event: Event) {
+        supabase.from("approved_events")
+            .insert(EventId(event.id))
+    }
+
+    suspend fun createEvent(event: EventInsert): Result<Unit> {
         return try {
             supabase.from("events").insert(event)
 
