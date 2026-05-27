e52daafb42ede1d5252faf7a55a715746015117f
matteo.crepaldi04 <matteo.crepaldi04@gmail.com>
2026-05-27 13:26:53 +0200

    Handled current position fetching, permissions and map navigating to user position

--- app/src/main/java/com/unibo/unievents/ui/screens/map/MapScreen.kt
+++ app/src/main/java/com/unibo/unievents/ui/screens/map/MapScreen.kt
@@ -1,5 +1,9 @@
 package com.unibo.unievents.ui.screens.map
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,16 +13,27 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.GpsFixed
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,17 +41,25 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.preference.PreferenceManager
+import com.unibo.unievents.data.Coordinates
 import com.unibo.unievents.data.Event
+import com.unibo.unievents.data.LocationService
 import com.unibo.unievents.ui.composables.BottomBar
 import com.unibo.unievents.ui.composables.TopBar
+import com.unibo.unievents.utils.PermissionStatus
+import com.unibo.unievents.utils.rememberMultiplePermissions
+import kotlinx.coroutines.launch
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 @Composable
 fun MapScreen(
@@ -44,6 +67,81 @@ fun MapScreen(
     actions: MapActions,
     navController: NavHostController
 ) {
+    var showLocationDisabledAlert by remember { mutableStateOf(false) }
+    var showPermissionDeniedAlert by remember { mutableStateOf(false) }
+    var showPermissionSnackbar by remember { mutableStateOf(false) }
+
+    val ctx = LocalContext.current
+    val locationService = remember { LocationService(ctx) }
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    val coordinates by locationService.coordinates.collectAsStateWithLifecycle()
+    val loadingLocation by locationService.isLoading.collectAsStateWithLifecycle()
+
+    fun getCurrentLocation() = scope.launch {
+        try {
+            locationService.getCurrentLocation()
+        } catch (_: IllegalStateException) {
+            showLocationDisabledAlert = true
+        }
+    }
+
+    val locationPermissions = rememberMultiplePermissions(
+        listOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    ) { statuses ->
+        when {
+            statuses.any { it.value == PermissionStatus.Granted } -> getCurrentLocation()
+            statuses.all { it.value == PermissionStatus.PermanentlyDenied } -> showPermissionSnackbar = true
+            else -> showPermissionDeniedAlert = true
+        }
+    }
+
+    fun getLocationOrRequestPermission() {
+        if (locationPermissions.statuses.any { it.value == PermissionStatus.Granted }) getCurrentLocation()
+        else locationPermissions.launchPermissionRequest()
+    }
+
+    fun openLocationSettings() {
+        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+
+        if (intent.resolveActivity(ctx.packageManager) != null) ctx.startActivity(intent)
+    }
+
+    if (showLocationDisabledAlert) {
+        LocationDisabledAlert(
+            onAction = { openLocationSettings() },
+            onDismiss = { showLocationDisabledAlert = false }
+        )
+    }
+
+    if (showPermissionDeniedAlert) {
+        PermissionDeniedAlert(
+            onAction = { locationPermissions.launchPermissionRequest() },
+            onDismiss = { showPermissionDeniedAlert }
+        )
+    }
+
+    PermissionPermanentlyDeniedSnackbar(
+        snackbarHostState,
+        show = showPermissionSnackbar,
+        onAction = {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                data = Uri.fromParts("package", ctx.packageName, null)
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            }
+            if (intent.resolveActivity(ctx.packageManager) != null) {
+                ctx.startActivity(intent)
+            }
+        },
+        onHide = { showPermissionSnackbar = false }
+    )
+
     Scaffold(
         topBar = { TopBar(navController, "Mappa eventi") },
         bottomBar = { BottomBar(navController) }
@@ -60,7 +158,18 @@ fun MapScreen(
                     .height(360.dp)
             ) {
                 Box {
-                    EventsMap(state.events, state.selectedEvent)
+                    EventsMap(state.events, state.selectedEvent, coordinates)
+
+                    IconButton(
+                        onClick = { getLocationOrRequestPermission() },
+                        colors = IconButtonDefaults.iconButtonColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                        ),
+                        enabled = !loadingLocation
+                    ) {
+                        Icon(Icons.Filled.GpsFixed, "Current location")
+                    }
                 }
             }
 
@@ -110,10 +219,12 @@ fun MapScreen(
 @Composable
 fun EventsMap(
     events: List<Event>,
-    selectedEvent: Pair<Double, Double>?
+    selectedEvent: Pair<Double, Double>?,
+    userLocation: Coordinates?
 ) {
     val ctx = LocalContext.current
     var lastSelection by remember { mutableStateOf<Pair<Double, Double>?>(null) }
+    var lastUserLocation by remember { mutableStateOf<Coordinates?>(null) }
 
     // Don't run this code at every recomposition, only once
     LaunchedEffect(Unit) {
@@ -133,11 +244,14 @@ fun EventsMap(
 
                 controller.setZoom(17.0)
 
-                if (selectedEvent == null) {
-                    controller.setCenter(GeoPoint(44.0633, 12.5707))
-                } else {
+                if (selectedEvent != null) {
                     controller.setCenter(GeoPoint(selectedEvent.first, selectedEvent.second))
                     lastSelection = selectedEvent
+                } else if (userLocation != null) {
+                    controller.setCenter(GeoPoint(userLocation.latitude, userLocation.longitude))
+                    lastUserLocation = userLocation
+                } else {
+                    controller.setCenter(GeoPoint(44.14771, 12.2356534))
                 }
             }
         },
@@ -155,6 +269,9 @@ fun EventsMap(
             if (selectedEvent != null && selectedEvent != lastSelection) {
                 mapView.controller.animateTo(GeoPoint(selectedEvent.first, selectedEvent.second))
                 lastSelection = selectedEvent
+            } else if (userLocation != null && userLocation != lastUserLocation) {
+                mapView.controller.animateTo(GeoPoint(userLocation.latitude, userLocation.longitude))
+                lastUserLocation = userLocation
             }
 
             mapView.invalidate()
@@ -192,6 +309,83 @@ fun EventCard(
                 text = address,
                 style = MaterialTheme.typography.bodyMedium
             )
+        }
+    }
+}
+
+@Composable
+fun LocationDisabledAlert(
+    onAction: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    AlertDialog(
+        title = { Text("La posizione è disattivata") },
+        text = { Text("La geolocalizzazione deve essere abilitata per poter ricavare la tua posizione attuale") },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onAction()
+                    onDismiss()
+                }
+            ) {
+                Text("Attiva")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Annulla")
+            }
+        },
+        onDismissRequest = onDismiss
+    )
+}
+
+@Composable
+fun PermissionDeniedAlert(
+    onAction: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    AlertDialog(
+        title = { Text("Accesso alla posizione negato") },
+        text = { Text("L'accesso alla posizione deve essere concesso per poter ricavare la tua posizione attuale") },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onAction()
+                    onDismiss()
+                }
+            ) {
+                Text("Concedi")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Annulla")
+            }
+        },
+        onDismissRequest = onDismiss
+
+    )
+}
+
+@Composable
+fun PermissionPermanentlyDeniedSnackbar(
+    snackbarHostState: SnackbarHostState,
+    show: Boolean,
+    onAction: () -> Unit,
+    onHide: () -> Unit
+) {
+    if (show) {
+        LaunchedEffect(snackbarHostState) {
+            val res = snackbarHostState.showSnackbar(
+                "Location permission is required.",
+                "Go to Settings",
+                duration = SnackbarDuration.Long
+            )
+            if (res == SnackbarResult.ActionPerformed) {
+                onAction()
+            }
+            onHide()
         }
     }
 }
