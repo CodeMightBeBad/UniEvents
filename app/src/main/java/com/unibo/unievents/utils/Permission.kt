8d6cb9089de2169ec36ffe9fc9232c4179da1f3e
emme004 <andrea.monti24@studio.unibo.it>
2026-06-04 10:07:56 +0200

    remove unused import

--- app/src/main/java/com/unibo/unievents/utils/Permission.kt
+++ app/src/main/java/com/unibo/unievents/utils/Permission.kt
@@ -5,7 +5,6 @@ import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
